bb1ea2d7c2dcdf15303852c275d073f5d76517b6
BHATT HIRAK <2502031830043@SILVEROAKUNI.AC.IN>
2026-05-26 13:29:50 +0530

    feat: update search route to include City query param

--- OJT/DAY12/index.js
+++ OJT/DAY12/index.js
@@ -6,5 +6,20 @@ app.listen(3000, () => {
 });
 
 app.get('/', (req, res) => {
-    res.send("Hello Bhattjii")
+    res.send("<h1>Hello Bhattjii</h1>")
+});
+app.get('/about', (req, res) => {
+    res.send("<h1>About page</h1>")
+});
+app.get('/about/user', (req, res) => {
+    res.send("<h1> User page</h1>")
+});
+app.get('/about/user/:userid/book/:bookid', (req, res) => {
+    res.send(`<h1>User ID: ${req.params.userid} & Book ID: ${req.params.bookid}</h1>`);
+});
+app.get('/search', (req, res) => {
+    const Name = req.query.Name;
+    const Age = req.query.Age;
+    const City = req.query.City;
+    res.send(`<h1>search results for Name : ${Name} , Age: ${Age} , City: ${City}</h1>`);
 });
